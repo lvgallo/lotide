9d1d7816103873f715eb8050dc8a0059a8192f83
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-11-23 01:24:59 +0000

    console.assert and emojis

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const thumbsUp = String.fromCodePoint(0x1F44D);
+  const thumbsDown = String.fromCodePoint(0x1F44E);
+
+  if (actual === expected) {
+    console.log(thumbsUp + 'Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log(thumbsDown + 'Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
