5ed16e031c4ebea00ffe001e8f219bde11492334
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-11-24 04:15:12 +0000

    returning false for length

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,6 @@
 const assertEqual = function(actual, expected) {
   const thumbsUp = String.fromCodePoint(0x1F44D);
   const thumbsDown = String.fromCodePoint(0x1F44E);
-
   if (actual === expected) {
     console.log(thumbsUp + ` Assertion Passed: ${actual}  ===  ${expected}`);
   } else {
@@ -10,20 +9,22 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1,array2){
-	if (array1.length === array2.length){
-		for (let i = 0; i <= array1.length; i++){
-			if(array1[i] !== array2[i]){
-				return false;
-			} 
-		}
-		return true;
-	}
-}
+const eqArrays = function(array1,array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i <= array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // TEST CODE
 /*assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);node 
+assertEqual(1, 1);node
 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
